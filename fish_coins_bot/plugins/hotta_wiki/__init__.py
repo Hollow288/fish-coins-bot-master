fdea9f7e663b4afe7e43a01fb1e6b00be4618c06
“hollow288” <571339274@qq.com>
2024-12-10 18:06:36 +0800

    willpower wiki

--- fish_coins_bot/plugins/hotta_wiki/__init__.py
+++ fish_coins_bot/plugins/hotta_wiki/__init__.py
@@ -1,5 +1,6 @@
 from .make_arms_img import arms_img_scheduled
 from .make_willpower_img import willpower_img_scheduled
 from .reply_arms_img import arms_img_handle_function
+from .reply_willpower_img import willpower_img_handle_function
 
-__all__ = ["arms_img_scheduled","arms_img_handle_function","willpower_img_scheduled",]  # 明确导出的函数
+__all__ = ["arms_img_scheduled","arms_img_handle_function","willpower_img_scheduled","willpower_img_handle_function"]  # 明确导出的函数
