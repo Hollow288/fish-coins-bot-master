6ba2054ba3979d6d2aa13fc5ee6577f29c28f6ca
“hollow288” <571339274@qq.com>
2024-12-16 17:42:00 +0800

    YuCoinsWeekly

--- fish_coins_bot/plugins/hotta_wiki/__init__.py
+++ fish_coins_bot/plugins/hotta_wiki/__init__.py
@@ -3,7 +3,7 @@ from .make_willpower_img import willpower_img_scheduled
 from .reply_arms_img import arms_img_handle_function
 from .reply_willpower_img import willpower_img_handle_function
 from .reply_yu_coins import yu_coins_type_img_handle_function, add_yu_coins_weekly_handle_function, \
-    yu_coins_weekly_img_handle_function
+    yu_coins_weekly_img_handle_function, flushed_yu_coins_weekly_handle_function
 
 __all__ = [
     "arms_img_scheduled",
@@ -12,5 +12,6 @@ __all__ = [
     "willpower_img_handle_function",
     "yu_coins_type_img_handle_function",
     "yu_coins_weekly_img_handle_function",
-    "add_yu_coins_weekly_handle_function"]  # 明确导出的函数
+    "add_yu_coins_weekly_handle_function",
+    "flushed_yu_coins_weekly_handle_function"]  # 明确导出的函数
 
