f8eeac07ebb8afed3c33297adf95fc54a86160db
“hollow288” <571339274@qq.com>
2025-01-06 10:56:06 +0800

    活动结束提醒

--- fish_coins_bot/plugins/hotta_wiki/__init__.py
+++ fish_coins_bot/plugins/hotta_wiki/__init__.py
@@ -4,7 +4,8 @@ from .make_event_consultation import event_consultation_scheduled
 from .make_willpower_img import willpower_img_scheduled
 from .reply_arms_attack_img import arms_attack_img_handle_function
 from .reply_arms_img import arms_img_handle_function
-from .reply_common import handle_poke_event, help_menu_handle_function, event_consultation_handle_function
+from .reply_common import handle_poke_event, help_menu_handle_function
+from .reply_event_consultation import event_consultation_handle_function, event_consultation_end_scheduled
 from .reply_nuo_coins import nuo_coins_weekly_img_handle_function, add_nuo_coins_weekly_handle_function, \
     delete_nuo_coins_weekly_handle_function, flushed_nuo_coins_weekly_handle_function, \
     nuo_coins_type_img_handle_function
@@ -43,5 +44,6 @@ __all__ = [
     "help_menu_handle_function",
     # 活动资讯
     "event_consultation_scheduled",
-    "event_consultation_handle_function"
+    "event_consultation_handle_function",
+    "event_consultation_end_scheduled"
 ]
---
+++ fish_coins_bot/plugins/hotta_wiki/reply_event_consultation.py
@@ -0,0 +1,78 @@
+import pytz
+from nonebot import on_notice, on_command
+from nonebot.adapters.onebot.v11 import Bot, Event, PokeNotifyEvent, GroupMessageEvent
+from nonebot.rule import Rule
+
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
+from pathlib import Path
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot import get_bot,require
+from datetime import datetime
+
+from fish_coins_bot.database.hotta.event_consultation import EventConsultation
+from fish_coins_bot.utils.model_utils import days_diff_from_now
+
+require("nonebot_plugin_apscheduler")
+
+from nonebot_plugin_apscheduler import scheduler
+
+
+def is_group_chat(event) -> bool:
+    return isinstance(event, GroupMessageEvent)
+
+event_consultation = on_command(
+    "活动资讯",
+    rule=Rule(is_group_chat),
+    aliases={"近期活动", "塔塔活动"},
+    priority=10,
+    block=True,
+)
+
+@event_consultation.handle()
+async def event_consultation_handle_function(args: Message = CommandArg()):
+    image_path = Path("/app/screenshots/common") / "event-consultation.png"
+
+    # 检查文件是否存在
+    if image_path.exists():
+        # 发送图片
+        image_message = MessageSegment.image(f"file://{image_path}")
+        await event_consultation.finish(image_message)
+    else:
+        await event_consultation.finish("哇哦,图片找不到了~")
+
+
+
+
+@scheduler.scheduled_job("cron", hour=12, minute=30, second=0, id="event_consultation_end_scheduled")
+async def event_consultation_end_scheduled():
+
+    tz = pytz.timezone("Asia/Shanghai")
+    current_time = datetime.now(tz)
+
+    are_info_list = await EventConsultation.filter(
+        del_flag="0",
+        consultation_start__lte=current_time,
+        consultation_end__gte=current_time
+    ).order_by("consultation_end").values(
+        "consultation_title",
+        "consultation_start",
+        "consultation_end"
+    )
+
+    is_need_send = False
+    result_msg = "以下活动即将结束:\n"
+
+    for info in are_info_list:
+        if days_diff_from_now(info["consultation_end"]) == 1:
+            result_msg += f" {info["consultation_title"]}\n"
+            is_need_send = True
+
+    result_msg += "执行者们注意活动时间哦..."
+
+    bot = get_bot()
+    group_list = await bot.get_group_list()
+
+    if is_need_send:
+        for group in group_list:
+            await bot.send_group_msg(group_id=group['group_id'], message=result_msg)
